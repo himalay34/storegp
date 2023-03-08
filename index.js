54a65d40e3e350ee0a66e0be87b002aac2064804
Ngangchill <ngangchill@gmail.com>
2023-03-08 23:22:20 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+
+const app = express()
+app.get('/',(req,res)=>{
+	res.send("voila")
+})
+
+app.listen(port, function () {
+  console.log('Your pouchdb is listening on port ' + port);
+});
