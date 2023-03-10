8ccf26344f1e2a5b71594369e17404e5beecf32f
Ngangchill <ngangchill@gmail.com>
2023-03-10 21:31:07 +0600

    gitignore

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ mongoose
 
 const Item = require('./models/Item');
 
-app.get('/', (req, res) => {
+app.get('/',(req, res) => {
+	res.send("voila, Nayeem")
+});
+app.get('/item', (req, res) => {
   Item.find()
     .then(items => res.render('index', { items }))
     .catch(err => res.status(404).json({ msg: 'No items found' }));
