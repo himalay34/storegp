4ed80337742ebeca274991c8c96a7b0fa5a99c03
Himalay <54813967+himalay34@users.noreply.github.com>
2024-09-21 14:47:51 +0600

    Update server.js
    .

--- server.js
+++ server.js
@@ -1,44 +1,11 @@
-const express = require('express');
-//const mongoose = require('mongoose');
+require("dotenv").config();
+const App = require("./src/app");
 
-const app = express();
+App.addService("init-passport");
 
-app.set('view engine', 'ejs');
+App.boot();
 
-app.use(express.urlencoded({ extended: false }));
-/*
-// Connect to MongoDB
-mongoose
-  .connect(
-    'mongodb://' + process.env.MONGODB_HOST + ':27017/docker-node-mongo',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
-
-const Item = require('./models/Item');
-*/
-app.get('/',(req, res) => {
-	res.send("hellow wolrd... whats up?")
-});
-/*
-app.get('/item', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
-});
-
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-    name: req.body.name
-  });
-
-  newItem.save().then(item => res.redirect('/'));
-});
-*/
-const port = 3000;
-
-app.listen(port, () => console.log('Server running...'));
+const app = App.express();
 
 process.on("uncaughtException", (error) => {
   console.error("Uncaught Exception:", error);
